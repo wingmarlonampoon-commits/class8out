5b7c134fb3aa86e1db4af8e5ad9747f46dcb9fc6
unknown <20114581@s.ubaguio.edu>
2026-09-02 13:04:34 -0400

    fixing routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { copyFileSync, existsSync } from 'fs'
 
 export default defineConfig({
-  plugins: [
-    react(),
 
-    {
-      name: 'github-pages-spa-fallback',
+  plugins: [react()],
 
-      closeBundle() {
-        const indexFile = resolve(__dirname, 'dist/index.html')
-        const fallbackFile = resolve(__dirname, 'dist/404.html')
+  base: '/class8out/',
 
-        if (existsSync(indexFile)) {
-          copyFileSync(indexFile, fallbackFile)
-        }
-      },
-    },
-  ],
-
-  base: '/class8out/',
 })
