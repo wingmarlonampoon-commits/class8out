50753fe740494d02603fd4377dc93fa9b297a9ab
unknown <20114581@s.ubaguio.edu>
2026-09-02 09:08:30 -0400

    Configure environment files21

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/class8out">
       <App />
     </BrowserRouter>
   </StrictMode>,
